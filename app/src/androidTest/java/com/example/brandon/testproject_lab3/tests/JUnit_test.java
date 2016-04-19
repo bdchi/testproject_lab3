80df32551e41f0b1dcfa0f0f3a93ae887e474e1b
bdchi <bdchi@ucsd.edu>
2016-04-19 16:18:52 -0700

    first commit - fixed failed test

---
+++ app/src/androidTest/java/com/example/brandon/testproject_lab3/tests/JUnit_test.java
@@ -0,0 +1,29 @@
+package tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import android.widget.TextView;
+
+import com.example.brandon.testproject_lab3.MainActivity;
+import com.example.brandon.testproject_lab3.R;
+
+/**
+ * Created by brandon on 4/19/2016.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    MainActivity mainActivity;
+    public JUnit_test(){
+
+        super(MainActivity.class);
+    }
+    public void test_first(){
+
+        mainActivity = getActivity();
+
+        TextView textView = (TextView) mainActivity.findViewById(R.id.tv1);
+        String tester = textView.getText().toString();
+
+        assertEquals("Hello World!", tester);
+    }
+}
